40f39ff6f7b974a90cf798ab5540797ff8adb29d
Ashwini Kumar <ashwinidroid@gmail.com>
2024-09-06 19:54:30 +0530

    changed env variable

--- src/routes/api/get-video-url/+server.ts
+++ src/routes/api/get-video-url/+server.ts
@@ -1,10 +1,10 @@
 import { error } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 import AWS from "aws-sdk";
-import {AWS_ACCESS_KEY,AWS_SECRET_ACCESS_KEY,AWS_REGION} from "$env/static/private"
+import {AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY,AWS_REGION} from "$env/static/private"
 
 AWS.config.update({
-  accessKeyId: AWS_ACCESS_KEY,
+  accessKeyId: AWS_ACCESS_KEY_ID,
   secretAccessKey: AWS_SECRET_ACCESS_KEY,
   region: AWS_REGION,
 });
