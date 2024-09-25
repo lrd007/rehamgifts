19972ca1b0a18d1c6a33d618a9066fcc5a6e592c
Ashwini Kumar <ashwinidroid@gmail.com>
2024-09-25 13:08:15 +0530

    fixed unauthorized admin redirect url

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -1,11 +1,12 @@
 // src/routes/admin/+page.server.ts
+import { base } from "$app/paths";
 import type { PageServerLoad } from "./$types";
 import { redirect } from "@sveltejs/kit";
 
 export const load = (async ({ locals }) => {
   // We'll keep the authentication check here
   if (!locals.isAdmin) {
-    throw redirect(303, "/login");
+    throw redirect(303, `${base}/`);
   }
 
   return {
